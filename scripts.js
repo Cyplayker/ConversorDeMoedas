7065feb0231ac322a18cb2ebb0441c28d2d27833
Cyplayker <ruancypla@gmail.com>
2026-02-05 12:59:54 -0300

    front do valor das moedas

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
 const button = document.getElementById('converted-button');
 
-const dolar = 5.0; // taxa de câmbio exemplo
+const dolar = 5.2; // taxa de câmbio exemplo
 
 function convertedValues() {
     const inputReais = document.getElementById('input-real').value;
+    const realValueText = document.getElementById('real-value-text');
+    const currencyValueText = document.getElementById('currency-value-text');
+
+    realValueText.innerHTML = inputReais;
+    currencyValueText.innerHTML = inputReais / dolar;
 
     const result = inputReais / dolar;
 
