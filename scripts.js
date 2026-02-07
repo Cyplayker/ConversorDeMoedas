aaedbdd17cec47579c3c4ab0de3141e7f68b9a77
Cyplayker <ruancypla@gmail.com>
2026-02-07 14:27:50 -0300

    troca de nomes euro e dolar

--- scripts.js
+++ scripts.js
@@ -1,18 +1,46 @@
 const button = document.getElementById('converted-button');
+const select = document.getElementById('currency-select');
 
-const dolar = 5.2; // taxa de câmbio exemplo
+
+const dolar = 5.2; 
 
 function convertedValues() {
     const inputReais = document.getElementById('input-real').value;
     const realValueText = document.getElementById('real-value-text');
     const currencyValueText = document.getElementById('currency-value-text');
 
-    realValueText.innerHTML = inputReais;
-    currencyValueText.innerHTML = inputReais / dolar;
-
     const result = inputReais / dolar;
+
+    // Formatando BRL
+    const realFormatado = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    }).format(inputReais);
+
+    // Formatando USD
+    const dolarFormatado = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    }).format(result);
+
+    // Mostrando na tela
+    realValueText.innerHTML = realFormatado;
+    currencyValueText.innerHTML = dolarFormatado;
 
     console.log(result);
 }
 
+changecurrency = () => { 
+    const currencyName = document.getElementById('currency-name');
+    const currencyImage = document.getElementById('currency-image');
+    if (select.value === '€ Euro') {
+        currencyName.innerHTML = 'Euro';
+        currencyImage.src = './assets/euro.png';
+    } else {
+        currencyName.innerHTML = 'Dólar americano';
+        currencyImage.src = './assets/eua.png';
+    }
+}
+
 button.addEventListener('click', convertedValues);
+select.addEventListener("change", changecurrency);
